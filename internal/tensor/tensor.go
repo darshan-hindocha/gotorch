cb821cc13539636ca62ba8a0dabda91b99f1af5c
D-hash-code <73314268+D-hash-code@users.noreply.github.com>
2022-04-10 19:02:39 +0100

    ughh

--- internal/tensor/tensor.go
+++ internal/tensor/tensor.go
@@ -2,19 +2,33 @@ package tensor
 
 type TensorOperations interface {
 	initialise()
-	zeros(*Tensor, []int)
+	Zeros()
 	reshape(*Tensor, []int)
 	view(*Tensor, []int)
 	squeeze(*Tensor, []int)
 }
 
 type Tensor struct {
-	Values []float64
-	Shape   []int
+	Values     map[int][]float32
+	Dimensions int
+	Shape      map[int]int
 }
 
-
 func (t *Tensor) Initialise() error {
-	
+
 	return nil
+}
+
+func (t *Tensor) Zeros(tensorShape map[int]int) error {
+	t.Shape = tensorShape
+	t.Dimensions = len(tensorShape)
+
+	values := make(map[int][]float32)
+
+	for i:=0; i < t.Dimensions; i++ {
+		array := [10]float32
+		var arraySlice []float32 = array
+		values[i] = arraySlice
+	}	
+
 }
